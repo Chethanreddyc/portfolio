9d4401ee536fe8525215bf24123bb5d321b56195
Chethanreddyc <chethanreddy0503@gmail.com>
2026-03-16 12:14:46 +0530

    feat: initial commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+// import React from 'react'
+
+function App() {
+  return (
+    <div className="flex min-h-screen text-white bg-black items-center justify-center">App</div>
+  )
+}
+
+export default App
